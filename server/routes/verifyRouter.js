7950bb74b9c10c6066ce786cf0b4d366ed2ef4d1
GnuesJ <48038447+GnuesJ@users.noreply.github.com>
2021-06-17 16:54:55 -0400

    voice recording

--- server/routes/verifyRouter.js
+++ server/routes/verifyRouter.js
@@ -3,7 +3,7 @@ const userController = require('../controllers/userController');
 const cookieController = require('../controllers/cookieController');
 
 const router = express.Router();
-
+let blob;
 
 router.post('/login', userController.verifyUser, cookieController.createCookie, (req,res,next) => {
   res.status(200).json(res.locals); // res.locals => body = {valid: true} 
@@ -13,6 +13,10 @@ router.get('/logout', cookieController.trashCookie,(req, res, next)=> {
   res.status(200).json();
 });
 
+router.post('/testingBlob', (req,res,next) => {
+  if(blob)
+  res.status(200).json();
+})
 
 
 
